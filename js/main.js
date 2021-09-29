f030cf42e9d93d9998584b6b0cceb2db928ecaf2
TemperTB <temper_tb@mail.ru>
2021-09-29 17:57:09 +0500

    Переделал на стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-function getRandomIntFromTo(from, to) {
+const getRandomIntFromTo = (from, to) => {
   if (from < 0 || to <= from) {
     return false;
   }
   from = Math.ceil(from);
   to = Math.floor(to);
   return Math.floor(Math.random() * (to - from)) + from;
-}
+};
 
-function getRandomIntFromToWithComma(from, to, countSignsAfterComma) {
+const getRandomIntFromToWithComma = (from, to, countSignsAfterComma) => {
   if (from < 0 || to <= from) {
     return false;
   }
   return +(Math.random() * (to - from)).toFixed(countSignsAfterComma) + from;
-}
+};
 
 getRandomIntFromTo(0, 4);
 getRandomIntFromToWithComma(0.4, 4.9, 3);

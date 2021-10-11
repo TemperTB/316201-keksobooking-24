b5b3c6c2f5b4cee0c8ebdbb929d65b0b37cede56
TemperTB <temper_tb@mail.ru>
2021-10-11 23:17:47 +0500

    Разбил на модули

---
+++ js/utils/random.js
@@ -0,0 +1,20 @@
+const getRandomIntFromTo = (from, to) => {
+  if (from < 0 || to <= from) {
+    return false;
+  }
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from)) + from;
+};
+
+const getRandomIntFromToWithComma = (from, to, countSignsAfterComma) => {
+  if (from < 0 || to <= from) {
+    return false;
+  }
+  return (Math.random() * (to - from) + from).toFixed(countSignsAfterComma);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomIntFromTo(0, elements.length - 1)];
+
+
+export { getRandomIntFromTo, getRandomIntFromToWithComma, getRandomArrayElement };

b8bf1fd5ad3aa27177840227e23dbd8f45d22af2
TemperTB <temper_tb@mail.ru>
2021-10-16 00:20:15 +0500

    Добавил создание карточки жилья на основе шаблона и данных из data.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-import { createAdverts } from './data.js';
+import { makePopupList } from './popup.js';
+//import { deactivateForm, activateForm } from './form.js';
 
-console.log(createAdverts);
+const mapCanvas = document.querySelector('#map-canvas');
+const popupList = makePopupList();
+const firstPopupItem = popupList.querySelectorAll('.popup');
+mapCanvas.appendChild(firstPopupItem[0]);
+
+// deactivateForm('ad-form');
+// activateForm('ad-form');
+// deactivateForm('map__filters');
+// activateForm('map__filters');
